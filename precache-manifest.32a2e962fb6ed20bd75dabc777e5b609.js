2b908dbb67524437db57e20da685cf332a7ec219
Wiktor Walczak <w.walczak93@gmail.com>
2020-06-06 17:05:14 +0200

    Updates

---
+++ precache-manifest.32a2e962fb6ed20bd75dabc777e5b609.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9944fcb86599ccc66e792b3414aa0b5f",
+    "url": "/photo-gallery/index.html"
+  },
+  {
+    "revision": "ad2642b98ab87b57340b",
+    "url": "/photo-gallery/static/css/main.caad3efe.chunk.css"
+  },
+  {
+    "revision": "e54072632cf1dbceba1f",
+    "url": "/photo-gallery/static/js/2.0b1faa42.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/photo-gallery/static/js/2.0b1faa42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad2642b98ab87b57340b",
+    "url": "/photo-gallery/static/js/main.9e0edf8b.chunk.js"
+  },
+  {
+    "revision": "5f817286aed988ac4c0b",
+    "url": "/photo-gallery/static/js/runtime-main.446b20b8.js"
+  }
+]);
